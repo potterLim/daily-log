56d88667810a8ee745c973845a0e622b7d4af81b
potterLim <potterLim0808@gmail.com>
2025-08-02 08:39:16 +0900

    feat(view): add Razor views for daily log and layout

---
+++ Views/DailyLog/Morning.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DailyLog.Views.DailyLog
+{
+    public class MorningModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
